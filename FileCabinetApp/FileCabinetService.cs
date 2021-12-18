e58deb03b4986fa80bf3b2b7d750c0b3cf7702f0
Pavel Yakovlevich <pasha.yakovlevich@gmail.com>
2021-12-18 18:52:12 +0300

    Add guard clauses to CreateRecord method.

--- FileCabinetApp/FileCabinetService.cs
+++ FileCabinetApp/FileCabinetService.cs
@@ -9,6 +9,26 @@ namespace FileCabinetApp
 
         public int CreateRecord(string firstName, string lastName, DateTime dateOfBirth, char gender, decimal weight, short stature)
         {
+            var guard = new Guard();
+
+            guard.IsNotNull(firstName, nameof(firstName))
+                .IsNotEmptyOrWhiteSpace(firstName, nameof(firstName))
+                .Requires(() => { return firstName.Length >= 2 && firstName.Length <= 60; }, $"{nameof(firstName)} lenght must be greate than 1 and less than 61.");
+
+            guard.IsNotNull(lastName, nameof(lastName))
+                .IsNotEmptyOrWhiteSpace(lastName, nameof(lastName))
+                .Requires(() => { return lastName.Length >= 2 && lastName.Length <= 60; }, $"{nameof(lastName)} lenght must be greate than 1 and less than 61.");
+
+            guard.Requires(
+                () => { return dateOfBirth.CompareTo(new DateTime(1950, 1, 1)) >= 0 && dateOfBirth.CompareTo(DateTime.Now) <= 0; },
+                $"{nameof(dateOfBirth)} must be greater than 01-Jan-1950 and less or equal to current date.");
+
+            guard.IsInRange(gender, new[] { 'M', 'F' });
+
+            guard.GreaterThan(weight, 0);
+
+            guard.GreaterThan(stature, 0);
+
             var record = new FileCabinetRecord
             {
                 Id = this.list.Count + 1,
