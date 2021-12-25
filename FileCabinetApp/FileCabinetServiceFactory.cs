471a35b02cddce5475c7a2153234caec4e8e95ea
Pavel Yakovlevich <pasha.yakovlevich@gmail.com>
2021-12-25 23:13:35 +0300

    Add validation-rules command line parameter.

---
+++ FileCabinetApp/FileCabinetServiceFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using FileCabinetApp.Services;
+
+namespace FileCabinetApp
+{
+    internal static class FileCabinetServiceFactory
+    {
+        internal static FileCabinetService Create(string name)
+        {
+            return name switch
+            {
+                "custom" => new FileCabinetCustomService(),
+                _ => new FileCabinetDefaultService()
+            };
+        }
+    }
+}
--- FileCabinetApp/Program.cs
+++ FileCabinetApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 using FileCabinetApp.Services;
 
 namespace FileCabinetApp
@@ -14,7 +16,14 @@ namespace FileCabinetApp
         private const int DescriptionHelpIndex = 1;
         private const int ExplanationHelpIndex = 2;
 
-        private static FileCabinetService fileCabinetService = new FileCabinetCustomService();
+        private static readonly string ValidationRulesAttributeName = "validation-rules";
+
+        private static readonly ProgramInputArgument[] ProgramDefinedArguments = new[]
+        {
+            new ProgramInputArgument(ValidationRulesAttributeName, "v", new[] { "default", "custom" }),
+        };
+
+        private static FileCabinetService fileCabinetService = new FileCabinetDefaultService();
 
         private static bool isRunning = true;
 
@@ -47,13 +56,25 @@ namespace FileCabinetApp
         public static void Main(string[] args)
         {
             Console.WriteLine($"File Cabinet Application, developed by {Program.DeveloperName}");
+
+            var readArgumentsResult = ReadInputArguments(args);
+
+            if (readArgumentsResult.Item1 is null)
+            {
+                Console.WriteLine(readArgumentsResult.Item2);
+                PrintReadArgumentsErrorMessage();
+                return;
+            }
+
+            HandleArguments(readArgumentsResult.Item1);
+
             Console.WriteLine(Program.HintMessage);
             Console.WriteLine();
 
             do
             {
                 Console.Write("> ");
-                var inputs = Console.ReadLine() !.Split(' ', 2);
+                var inputs = Console.ReadLine()!.Split(' ', 2);
                 const int commandIndex = 0;
                 var command = inputs[commandIndex];
 
@@ -76,6 +97,34 @@ namespace FileCabinetApp
                 }
             }
             while (isRunning);
+        }
+
+        private static void HandleArguments(Dictionary<string, string> inputArguments)
+        {
+            if (inputArguments.ContainsKey(ValidationRulesAttributeName))
+            {
+                var argumentValue = inputArguments[ValidationRulesAttributeName].ToLower();
+
+                fileCabinetService = FileCabinetServiceFactory.Create(argumentValue);
+
+                Console.WriteLine($"Using {argumentValue} validation rules.");
+            }
+            else
+            {
+                Console.WriteLine($"Using default validation rules.");
+            }
+        }
+
+        private static void PrintReadArgumentsErrorMessage()
+        {
+            StringBuilder errorMessage = new StringBuilder("Defined arguments:\n");
+
+            foreach (var definedArgument in ProgramDefinedArguments)
+            {
+                errorMessage.AppendLine($"\t--{definedArgument.Name}(-{definedArgument.Abbreviation}) : {definedArgument.GetValidValuesString()}");
+            }
+
+            Console.WriteLine(errorMessage);
         }
 
         private static void PrintMissedCommandInfo(string command)
@@ -305,6 +354,106 @@ namespace FileCabinetApp
             {
                 Console.WriteLine($"#{record}");
             }
+        }
+
+        private static Tuple<Dictionary<string, string>?, string> ReadInputArguments(string[] args)
+        {
+            var result = new Dictionary<string, string>();
+
+            var definedArgumentIndex = 0;
+            var currentArgsElementIndex = 0;
+            while (currentArgsElementIndex < args.Length && definedArgumentIndex < ProgramDefinedArguments.Length)
+            {
+                var readArgumentResult = ReadInputArgument(args, ref currentArgsElementIndex);
+
+                if (!readArgumentResult.Item3.Equals(string.Empty))
+                {
+                    return new Tuple<Dictionary<string, string>?, string>(null, readArgumentResult.Item3);
+                }
+
+                readArgumentResult = PreprocessInputArgument((readArgumentResult.Item1, readArgumentResult.Item2));
+
+                if (!readArgumentResult.Item3.Equals(string.Empty))
+                {
+                    return new Tuple<Dictionary<string, string>?, string>(null, readArgumentResult.Item3);
+                }
+
+                result.Add(readArgumentResult.Item1, readArgumentResult.Item2);
+                definedArgumentIndex++;
+            }
+
+            return new Tuple<Dictionary<string, string>?, string>(result, string.Empty);
+        }
+
+        private static (string, string, string) PreprocessInputArgument((string, string) argumentData)
+        {
+            var argumentName = argumentData.Item1;
+            foreach (var definedArgument in ProgramDefinedArguments)
+            {
+                if (argumentName.Equals(definedArgument.Name, StringComparison.InvariantCultureIgnoreCase) ||
+                    argumentName.Equals(definedArgument.Abbreviation, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    var argumentValue = argumentData.Item2;
+
+                    if (definedArgument.ValidateValue(argumentValue))
+                    {
+                        return (definedArgument.Name, argumentValue, string.Empty);
+                    }
+
+                    return (argumentData.Item1, argumentData.Item2, $"Invalid value '{argumentValue}' for the '{argumentName}' argument.");
+                }
+            }
+
+            return (argumentData.Item1, argumentData.Item2, $"Argument '{argumentName}' is not defined.");
+        }
+
+        private static (string, string, string) ReadInputArgument(string[] args, ref int currentArgsIndex)
+        {
+            (string, string, string) result = (string.Empty, string.Empty, string.Empty);
+
+            var currentArgument = args[currentArgsIndex];
+            var trimmedArgument = currentArgument.TrimStart('-');
+
+            if (string.IsNullOrEmpty(trimmedArgument))
+            {
+                result.Item3 = $"Missing argument name for the input argument[{currentArgsIndex}]";
+                return result;
+            }
+
+            if (currentArgument.StartsWith("--"))
+            {
+                var splittedData = trimmedArgument.Split('=');
+
+                if (splittedData.Length < 2)
+                {
+                    result.Item3 = $"Missing value for the '{trimmedArgument}' argument.";
+                    return result;
+                }
+
+                result.Item1 = splittedData[0];
+                result.Item2 = splittedData[1];
+
+                currentArgsIndex++;
+            }
+            else if (currentArgument.StartsWith('-'))
+            {
+                if (currentArgsIndex + 1 == args.Length)
+                {
+                    result.Item3 = $"Missing value for the '{trimmedArgument}' argument.";
+                    return result;
+                }
+
+                result.Item1 = trimmedArgument;
+                result.Item2 = args[currentArgsIndex + 1];
+
+                currentArgsIndex += 2;
+            }
+            else
+            {
+                result.Item3 = $"Invalid parameter format: '{trimmedArgument}'";
+            }
+
+            return result;
         }
     }
 }
