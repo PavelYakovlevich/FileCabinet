16d0190be56cd822974ca39d0073062d059511a3
Pavel Yakovlevich <pasha.yakovlevich@gmail.com>
2022-01-01 21:19:40 +0300

    Add documentation for DumpSliceAttribute

--- FileCabinetApp/Serialization/DumpSliceAttribute.cs
+++ FileCabinetApp/Serialization/DumpSliceAttribute.cs
@@ -1,17 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace FileCabinetApp.Serialization
 {
+    /// <summary>
+    ///     Specifies extra memory areas, which can be placed before an serialized object.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
     public class DumpSliceAttribute : Attribute
     {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="DumpSliceAttribute"/> class.
+        /// </summary>
         public DumpSliceAttribute()
         {
             this.PrecedingAreaName = string.Empty;
             this.PrecedingAreaSize = 0;
         }
 
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="DumpSliceAttribute"/> class.
+        /// </summary>
+        /// <param name="precedingSpaceName">Name of preceding extra memory area.</param>
+        /// <param name="precedingSpaceSize">Size of preceding extra memory area.</param>
         public DumpSliceAttribute(string precedingSpaceName, int precedingSpaceSize)
             : this()
         {
@@ -21,8 +31,16 @@ namespace FileCabinetApp.Serialization
             this.PrecedingAreaSize = precedingSpaceSize;
         }
 
+        /// <summary>
+        /// Gets a name of preceding extra memory area.
+        /// </summary>
+        /// <value>Name of preceding extra memory area.</value>
         public string PrecedingAreaName { get; }
 
+        /// <summary>
+        /// Gets a size of preceding extra memory area.
+        /// </summary>
+        /// <value>Size of preceding extra memory area.</value>
         public int PrecedingAreaSize { get; }
     }
 }
