df96e31599ef047cda7c691246f325c04c7c08a8
Pavel Yakovlevich <pasha.yakovlevich@gmail.com>
2022-01-05 22:15:24 +0300

    Add documentation for all members in ProgramInputHandling class lib

--- ProgramInputHandling/ValueInputParameter.cs
+++ ProgramInputHandling/ValueInputParameter.cs
@@ -4,6 +4,9 @@ using System.Text.RegularExpressions;
 
 namespace ProgramInputHandling
 {
+    /// <summary>
+    ///     Class for a generic value parameter.
+    /// </summary>
     public class ValueInputParameter : AbstractInputParameter
     {
         private static readonly Dictionary<ArgumentType, Predicate<string>> ValidationFunctions = new Dictionary<ArgumentType, Predicate<string>>()
@@ -15,6 +18,14 @@ namespace ProgramInputHandling
 
         private Predicate<string> validationFunc;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ValueInputParameter"/> class.
+        /// </summary>
+        /// <param name="name">Name of the parameter.</param>
+        /// <param name="abbreviation">Shortcut fo the parameter's name.</param>
+        /// <param name="isMandatory">Specifies if paramter is mandatory or not.</param>
+        /// <param name="argumentType">Specifies type of arguments value.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="name"/> or <paramref name="abbreviation"/> is empty or whitespace or null.</exception>
         public ValueInputParameter(string name, string abbreviation, bool isMandatory, ArgumentType argumentType)
             : base(name, abbreviation, isMandatory)
         {
