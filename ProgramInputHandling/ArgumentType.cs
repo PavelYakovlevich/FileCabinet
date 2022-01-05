cb0fac25391e6f0e2efb1dc6465197416a036d91
Pavel Yakovlevich <pasha.yakovlevich@gmail.com>
2022-01-05 22:15:24 +0300

    Add documentation for all members in ProgramInputHandling class lib

--- ProgramInputHandling/ArgumentType.cs
+++ ProgramInputHandling/ArgumentType.cs
@@ -1,9 +1,21 @@
 ﻿namespace ProgramInputHandling
 {
+    /// <summary>
+    ///     Enum for representing a type of the paramters value.
+    /// </summary>
     public enum ArgumentType
     {
+        /// <summary>
+        ///     Parameter's value is an integer.
+        /// </summary>
         Integer,
+        /// <summary>
+        ///     Parameter's value is a file path.
+        /// </summary>
         FilePath,
+        /// <summary>
+        ///     Parameter's value is an positive integer.
+        /// </summary>
         PositiveInteger
     }
 }
