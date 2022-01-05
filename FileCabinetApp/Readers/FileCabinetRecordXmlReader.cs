ec4a7705296ea3f3a002677ee26114b61bfad9b7
Pavel Yakovlevich <pasha.yakovlevich@gmail.com>
2022-01-05 22:16:04 +0300

    Add documentation for all members in FileCabinetApp

--- FileCabinetApp/Readers/FileCabinetRecordXmlReader.cs
+++ FileCabinetApp/Readers/FileCabinetRecordXmlReader.cs
@@ -4,10 +4,17 @@ using System.Xml.Serialization;
 
 namespace FileCabinetApp.Readers
 {
+    /// <summary>
+    ///     Class for a reading <see cref="FileCabinetRecord"/> objects from a xml file.
+    /// </summary>
     public class FileCabinetRecordXmlReader : IFileCabinetRecordReader
     {
         private StreamReader reader;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileCabinetRecordXmlReader"/> class.
+        /// </summary>
+        /// <param name="reader"><see cref="StreamReader"/> object, from which records will be read.</param>
         public FileCabinetRecordXmlReader(StreamReader reader)
         {
             Guard.ArgumentIsNotNull(reader, nameof(reader));
--- FileCabinetApp/Readers/IFileCabinetRecordReader.cs
+++ FileCabinetApp/Readers/IFileCabinetRecordReader.cs
@@ -1,13 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace FileCabinetApp.Readers
 {
+    /// <summary>
+    ///     Interface for records readers.
+    /// </summary>
     public interface IFileCabinetRecordReader
     {
+        /// <summary>
+        ///     Reads all records from a file.
+        /// </summary>
+        /// <returns><see cref="IList{FileCabinetRecord}"/> object with all records, extracted from a file.</returns>
         IList<FileCabinetRecord> ReadAll();
     }
 }
