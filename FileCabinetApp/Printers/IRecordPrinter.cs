50fd86ef70556c6bf737d00a32218213bef54e36
Pavel Yakovlevich <pasha.yakovlevich@gmail.com>
2022-01-08 22:16:23 +0300

    Create IRecordPrinter, DefaultRecordPrinter.

---
+++ FileCabinetApp/Printers/IRecordPrinter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace FileCabinetApp.Printers
+{
+    /// <summary>
+    ///     Interface for printers of <see cref="FileCabinetApp"/> objects.
+    /// </summary>
+    public interface IRecordPrinter
+    {
+        /// <summary>
+        ///     Prints all <paramref name="records"/>.
+        /// </summary>
+        /// <param name="records">Records to print.</param>
+        void Print(IEnumerable<FileCabinetRecord> records);
+    }
+}
